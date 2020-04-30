7f421861ce33e2e12a6487e40eeb165da54fa41f
Bruna Silva <bruna-m.silva@hotmail.com>
2020-04-30 18:21:08 -0300

    Aula4 - Trabalhando com POST(-Criando as classes: TopicoForm e CursoRepository - Incluindo a anotação: PostMapping e ResponseBody).

--- forum/src/main/java/br/com/alura/forum/controller/TopicoController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicoController.java
@@ -1,14 +1,18 @@
 package br.com.alura.forum.controller;
 
 import br.com.alura.forum.controller.DTO.TopicoDTO;
+import br.com.alura.forum.controller.Form.TopicoForm;
 import br.com.alura.forum.model.Curso;
 import br.com.alura.forum.model.Topico;
+import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.persistence.Id;
+import java.net.URI;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -19,6 +23,10 @@ public class TopicoController {
 
     @Autowired
     private TopicoRepository repository;
+
+    @Autowired
+    private CursoRepository cursoRepository;
+    private TopicoForm form;
 
     @GetMapping
     public List<TopicoDTO> lista(String nomeCurso) {
@@ -36,6 +44,17 @@ public class TopicoController {
         return repository.findByCursoId(id)
                 .map(c -> ResponseEntity.ok().body(c))
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseBody
+    public ResponseEntity<TopicoDTO> cadastrar(TopicoForm form,
+                                               UriComponentsBuilder uriBuilder) {
+        this.form = form;
+        Topico topico = this.form.converter(cursoRepository);
+        repository.save(topico);
+        URI uri = uriBuilder.path("topicos/{id}").buildAndExpand(topico.getId()).toUri();
+        return ResponseEntity.created(uri).body(new TopicoDTO(topico));
     }
 
 }
