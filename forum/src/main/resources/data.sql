ec1606db52278014cbb3ab296f2c70222c19ee2d
Bruna Silva <bruna-m.silva@hotmail.com>
2020-05-11 19:43:21 -0300

    2-Aula3 -  Proteção com Spring Security(-Habilitando o Spring Security: 1° incluindo a dependência 'spring-boot-starter-security' no pom.xml 2° criando a classe: SecurityConfigurations 3° incluindo as anotações 'SecurityConfigurations' e ‘Configuration’ -Liberando acesso aos endpoints públicos: 1° incluindo os três métodos ‘configure’ da classe abstrata WebSecurityConfigurerAdapter, que foi extendida a classe SecurityConfigurations: AuthenticationManagerBuilder, HttpSecurity e WebSecurity -Restringindo o acesso aos endpoints privados: 1° Criando a classe entidade Perfil 2° implementando o ‘GrantedAuthority’ a classe Perfil 3° implementando o método ‘getAuthority’ 4° criando as classes ‘AutenticacaoService’ e ‘UsuarioRepository’ 5° implementando a classe ‘UserDetailsService’ e implementando o método ‘loadUserByUsername’).

--- forum/src/main/resources/data.sql
+++ forum/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$UWgHEz7GbEWJ8/XqVlq9POuuCT0qlD7JeKE.682xFdXSgu4PLfYma');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
